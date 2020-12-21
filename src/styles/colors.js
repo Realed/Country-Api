f76e3d65a302393fb6bbd40ce5f3357fa4a2aa96
DanielGT <danielgonzalezt13@gmail.com>
2020-12-21 11:20:28 +0100

    New color palette

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  darkMode: {
+    backColor: "rgb(100, 100, 100, 1)",
+    backColorAccent: "rgb(30, 30, 30, 1)",
+    color: "rgb(215, 215, 215, 1)",
+    colorAccent: "white",
+  },
+  lightMode: {
+    backColor: "#f7f5f5",
+    backColorAccent: "white",
+    color: "#333",
+    colorAccent: "black",
+  },
+}
+
+export default colors
